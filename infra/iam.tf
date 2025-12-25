e80910fc1c150b98cf291fd2a7c7eef2a35cc17c
Jesben <56515901+jesbensevi@users.noreply.github.com>
2025-12-25 16:18:58 -0500

    feat(iam.tf): add service account permissions for External Secrets and Secret Manager access refactor(iam.tf): update service account name in workload identity binding for clarity and correctness

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,15 +1,25 @@
-# 1. Creamos la identidad (Service Account)
+# -----------------------------------------------------------------------------
+# External Secrets - Service Account y permisos
+# -----------------------------------------------------------------------------
+
 resource "google_service_account" "external_secrets" {
   account_id   = "external-secrets-sa"
   display_name = "External Secrets Service Account"
 }
 
-
+# Binding de Workload Identity (KSA -> GSA)
 resource "google_service_account_iam_binding" "workload_identity_binding" {
   service_account_id = google_service_account.external_secrets.name
   role               = "roles/iam.workloadIdentityUser"
 
   members = [
-    "serviceAccount:${var.project_id}.svc.id.goog[external-secrets/external-secrets]"
+    "serviceAccount:${var.project_id}.svc.id.goog[external-secrets/external-secrets-sa]"
   ]
+}
+
+# Permiso para leer secretos de Secret Manager
+resource "google_project_iam_member" "external_secrets_secret_accessor" {
+  project = var.project_id
+  role    = "roles/secretmanager.secretAccessor"
+  member  = "serviceAccount:${google_service_account.external_secrets.email}"
 }
